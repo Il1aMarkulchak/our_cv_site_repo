c98708d0ef7981466db49c4a550726535591ec78
Il1aMarkulchak <ilya.markulchak@mail.ru>
2023-08-02 23:28:45 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React together
         </a>
       </header>
     </div>
